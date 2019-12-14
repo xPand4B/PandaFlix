e3e2ba0f12ee002c08466d32c2527599ecbc146d
Eric Heinzl <e.heinzl@shopware.com>
2019-12-14 13:56:47 +0100

    Updated dev-ops commands

--- dev-ops/common/actions/.cache.sh
+++ dev-ops/common/actions/.cache.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 
-I: php artisan clear-compiled
-I: php artisan cache:clear
-I: php artisan config:cache
 I: php artisan optimize:clear
-I: php artisan route:clear
-I: php artisan view:clear
