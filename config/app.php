bf0a2a26b3ce5f7d0ea9aef3a64453571a4229ca
Eric Heinzl <e.heinzl@shopware.com>
2019-12-14 13:59:36 +0100

    Added PandaFlix config file

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'xPand Netflix'),
+    'name' => env('APP_NAME', 'PandaFlix'),
 
     /*
     |--------------------------------------------------------------------------
---
+++ config/pandaflix.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Component Path
+    |--------------------------------------------------------------------------
+    |
+    | This will define were the components root directory is located.
+    |
+    */
+
+    'component_path' => 'app/Components',
+];
