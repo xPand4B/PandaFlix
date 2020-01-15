2f599ba5b0b6277dd19921537028cc9de7b0abcc
Eric Heinzl <e.heinzl@shopware.com>
2020-01-15 08:14:32 +0100

    Refactoring

--- app/Components/Common/Helper/ComponentHelper.php
+++ app/Components/Common/Helper/ComponentHelper.php
@@ -7,23 +7,49 @@ use Illuminate\Support\Facades\File;
 
 class ComponentHelper
 {
-
-    public static function getCount(): int
+    /**
+     * Returns a count of all components.
+     *
+     * @param bool $includeCommon
+     * @return int
+     */
+    public static function getCount(bool $includeCommon = false): int
     {
-        return sizeof(self::getComponentDirectories());
+        return sizeof(self::getComponentDirectories($includeCommon));
+    }
+
+    /**
+     * Returns all component names.
+     *
+     * @param string $additionalPath
+     * @param bool $includeCommon
+     * @return array
+     */
+    public static function getComponentNames(string $additionalPath = null, bool $includeCommon = false): array
+    {
+        $directories = self::getComponentDirectories($includeCommon);
+
+        if ($additionalPath) {
+            foreach ($directories as $key => $directory) {
+                $directories[$key] = $directory . DIRECTORY_SEPARATOR . $additionalPath;
+            }
+        }
+
+        return $directories;
     }
 
     /**
      * Returns all component files matching the search term.
      *
      * @param string $search
+     * @param bool $includeCommon
      * @return array
      */
-    public static function getFilesByName(string $search): array
+    public static function getFilesByName(string $search, bool $includeCommon = false): array
     {
         $files = [];
 
-        foreach (self::getComponentDirectories() as $component_directory) {
+        foreach (self::getComponentDirectories($includeCommon) as $component_directory) {
             $component_files = File::allFiles($component_directory);
             foreach ($component_files as $file) {
                 if (strpos($file->getFilename(), $search) !== false) {
@@ -36,13 +62,18 @@ class ComponentHelper
     }
 
     /**
-     * Get all component directories except Common.
+     * Get all component directories .
      *
+     * @param bool $includeCommon
      * @return array
      */
-    private static function getComponentDirectories(): array
+    private static function getComponentDirectories(bool $includeCommon = false): array
     {
         $components = File::directories(PandaFlix::ComponentPath());
+
+        if ($includeCommon) {
+            return $components;
+        }
 
         foreach ($components as $key => $component) {
             if (strpos($component, 'Common')) {
